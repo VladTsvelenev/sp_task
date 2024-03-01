f10740538416999b0ca1c4cf4fd13fc30c022335
VladTsvelenev <89857655422@mail.ru>
2024-03-01 15:34:37 +0300

    test

--- item1/sp_project.py
+++ item1/sp_project.py
@@ -13,32 +13,40 @@ import torch.nn.functional as F
 from sklearn.metrics import accuracy_score 
 import csv
 import warnings 
+from torch.utils.tensorboard import SummaryWriter
+from cData import ImageDataset
 
 os.chdir(os.getcwd())
 warnings.filterwarnings('ignore')
-from cData import ImageDataset
 
 transform1 = transforms.Compose([         
             v2.ToTensor(),
             v2.Resize([150, 150])])
 
 train_data = ImageDataset(annotations_file = 'table.csv', img_dir = 'test_data', transform=transform1)
-train_size = int(len(train_data) * 0.8)
-test_size = len(train_data) - train_size
+train_size = int(len(train_data) * 0.70)
+val_size = int(len(train_data) * 0.20)
+test_size = len(train_data) - train_size - val_size
 
-train_data, test_data = torch.utils.data.random_split(train_data, [train_size, test_size])
+
+train_data, val_data, test_data = torch.utils.data.random_split(train_data, [train_size, val_size, test_size])
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=20, shuffle=True)
+val_loder = torch.utils.data.DataLoader(val_data, batch_size=20, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=20, shuffle=False)
 
 
 train_dataD = ImageDataset(annotations_file = 'other3.csv', img_dir = 'test_data', transform=transform1)
-train_sizeD = int(len(train_dataD) * 0.8)
-test_sizeD = len(train_dataD) - train_sizeD
+train_sizeD = int(len(train_dataD) * 0.70)
+val_sizeD = int(len(train_dataD) * 0.20)
+test_sizeD = len(train_dataD) - train_sizeD - val_sizeD
 
-train_dataD, test_dataD = torch.utils.data.random_split(train_dataD, [train_sizeD, test_sizeD])
+train_dataD, val_dataD, test_dataD = torch.utils.data.random_split(train_dataD, [train_sizeD, val_sizeD, test_sizeD])
+
 train_loaderD = torch.utils.data.DataLoader(train_dataD, batch_size=20, shuffle=True)
+val_loderD = torch.utils.data.DataLoader(val_dataD, batch_size=20, shuffle=True)
 test_loaderD = torch.utils.data.DataLoader(test_dataD, batch_size=20, shuffle=False)
+
 
 
 class ConvNet(nn.Module):
@@ -98,8 +106,9 @@ def evaluate(model, dataloader, loss_fn):
     return accuracy, np.mean(losses)
 
 
-def train(model, loss_fn, optimizer, n_epoch, load):
-    maxaccuracy = 0
+def train(model, loss_fn, optimizer, n_epoch, load, val):
+    num_iter = 0
+    minloss = 0
     for epoch in range(n_epoch):
 
         model.train(True)
@@ -119,21 +128,26 @@ def train(model, loss_fn, optimizer, n_epoch, load):
             model_answers = torch.argmax(logits, dim=1)
             train_accuracy = torch.sum(y_batch == model_answers.cpu()) / len(y_batch)
             running_accuracies.append(train_accuracy)
+            num_iter += 1
+
+        val_accuracy, val_loss = evaluate(model, val, loss_fn=loss_fn)
             
         model.train(False)
         accuracy = (sum(running_accuracies) * 100 / len(running_accuracies)).numpy()
-        print("Epoch:", str(epoch+1) + ", accuracy:", accuracy)
-        if accuracy >= maxaccuracy:
+        losst = sum(running_losses) * 100 / len(running_losses)
+        print("Epoch:", str(epoch+1))
+        print("Train | accuracy:", accuracy, ", loss:", losst)
+        print("Validation | accuracy:", val_accuracy.numpy() * 100, ", loss:", val_loss)
+        print("__________________________________________________________")
+        if val_loss <= minloss:
             torch.save(model.state_dict(), './model.pt')
-            maxaccuracy = accuracy
+            minloss = val_loss
         
     return model
 
 def create_model(model, num_freeze_layers, num_out_classes):
-    # замена последнего слоя сети
     model.fc3 = nn.Linear(512, num_out_classes)
-    model.load_state_dict(checkpoint)
-    # заморозка слоев
+    model.load_state_dict(torch.load('model.pt'))
     for i, layer in enumerate(model.children()):
         if i < num_freeze_layers:
             for param in layer.parameters():
@@ -148,13 +162,13 @@ learning_rate = 1e-3
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
-model = train(model, loss_fn, optimizer, 10, train_loader)
-model.load_state_dict(torch.load('model.pt'))
+model = train(model, loss_fn, optimizer, 10, train_loader, val_loder)
 
 model = create_model(model, 3, 5)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-model = train(model, loss_fn, optimizer, 20, train_loaderD)
+model = train(model, loss_fn, optimizer, 20, train_loaderD, val_loderD)
 model.load_state_dict(torch.load('model.pt'))
+
 
 train_accuracy, _ = evaluate(model, train_loaderD, loss_fn)
 print('Train accuracy:', train_accuracy.numpy())
